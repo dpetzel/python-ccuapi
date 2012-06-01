f2dbef9f7167878f2a11943718a560bf4c57744e
Daniel Ryan <dryan@dryan.com>
2012-05-31 21:57:29 -0500

    initial working getting a purge working

---
+++ akamai/__init__.py
@@ -0,0 +1,50 @@
+import os,sys,ConfigParser
+
+django_settings =   False
+try:
+    from django.conf import settings as django_settings
+except ImportError:
+    pass
+
+class AkamaiException(Exception):
+    pass
+    
+class AkamaiConfigException(Exception):
+    pass
+
+AKAMAI_USERNAME     =   os.environ.get('AKAMAI_USERNAME', None)
+AKAMAI_PASSWORD     =   os.environ.get('AKAMAI_PASSWORD', None)
+AKAMAI_NOTIFY_EMAIL =   os.environ.get('AKAMAI_NOTIFY_EMAIL', None)
+
+if django_settings:
+    try:
+        AKAMAI_USERNAME     =   django_settings.get('AKAMAI_USERNAME', AKAMAI_USERNAME)
+        AKAMAI_PASSWORD     =   django_settings.get('AKAMAI_PASSWORD', AKAMAI_PASSWORD)
+        AKAMAI_NOTIFY_EMAIL =   django_settings.get('AKAMAI_NOTIFY_EMAIL', AKAMAI_NOTIFY_EMAIL)
+    except ImportError:
+        pass # this isn't being run inside a Django environment
+
+config  =   ConfigParser.SafeConfigParser()
+config.readfp(open(os.path.expanduser('~/.akamai')))
+
+if not AKAMAI_USERNAME or not AKAMAI_PASSWORD:
+    if os.path.exists(os.path.expanduser('~/.akamai')):
+        if config.has_section('Credentials'):
+            if config.has_option('Credentials', 'username'):
+                AKAMAI_USERNAME =   config.get('Credentials', 'username')
+            else:
+                raise AkamaiConfigException('.akamai config is missing username')
+            if config.has_option('Credentials', 'password'):
+                AKAMAI_PASSWORD =   config.get('Credentials', 'password')
+            else:
+                raise AkamaiConfigException('.akamai config is missing username')
+        else:
+            raise AkamaiConfigException('.akamai config is missing the Credentials section')
+    else:
+        raise AkamaiConfigException('Akamai credentials were not found in either enivronment variables or in a .akamai config file')
+        
+if not AKAMAI_NOTIFY_EMAIL and config.has_section('Notifications') and config.has_option('Notifications', 'email'):
+    AKAMAI_NOTIFY_EMAIL =   config.get('Notifications', 'email')
+
+from . import purge
+__all__ =   ['purge',]
---
+++ akamai/purge.py
@@ -0,0 +1,78 @@
+from akamai import AKAMAI_USERNAME, AKAMAI_PASSWORD, AKAMAI_NOTIFY_EMAIL, AkamaiException
+
+import urllib, os
+from suds.client import Client
+from suds.xsd import doctor
+
+QuerySet    =   None
+try:
+    from django.db.models.query import QuerySet
+except ImportError:
+    pass
+
+class PurgeRequest(object):
+    
+    def __init__(self, username = AKAMAI_USERNAME, password = AKAMAI_PASSWORD, email = AKAMAI_NOTIFY_EMAIL, options = None, urls = None, wsdl = None):
+        
+        self.wsdl   =   wsdl
+        if not self.wsdl:
+            self.wsdl       =   os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ccuapi-axis.wsdl')
+            imp             =   doctor.Import('http://schemas.xmlsoap.org/soap/encoding/')
+            imp.filter.add('http://www.akamai.com/purge')
+            impdoc          =   doctor.ImportDoctor(imp)
+            self.client     =   Client("file://%s" % self.wsdl, doctor = impdoc)
+        self.username   =   username
+        if not self.username:
+            raise AkamaiException('Username not provided')
+        self.password   =   password
+        if not self.password:
+            raise AkamaiException('Password not provided')
+        self.email      =   email
+            
+        self.options    =   {
+            'email-notification-name':  self.email,
+            'action':                   'remove',
+            'type':                     'arl',
+            'domain':                   'production',
+        }
+        if options:
+            self.options.update(options)
+            
+        self.urls   =   []
+        if urls is not None:
+            self.add(urls)
+            
+        self.results        =   []
+        
+    def add(self, urls = None):
+        if urls is None:
+            raise AkamaiException('urls must be a string, list of strings, Django QuerySet or Django object')
+            
+        if isinstance(urls, list):
+            self.urls.extend(urls)
+        elif isinstance(urls, basestring):
+            self.urls.append(urls)
+        elif QuerySet and isinstance(urls, QuerySet):
+            for obj in urls:
+                self.add(obj)
+        elif hasattr(urls, 'get_absolute_url'):
+            self.urls.append(urls.get_absolute_url())
+        else:
+            raise AkamaiException("Don't know how to handle %r" % urls)
+            
+    def purge(self):
+        self.results    =   []
+        num_urls        =   len(self.urls)
+        while len(self.urls):
+            urls            =   self.urls[:100]
+            self.urls       =   self.urls[100:]
+            if urls:
+                self.results.append((self.client.service.purgeRequest(
+                    self.username,
+                    self.password,
+                    '',
+                    ["%s=%s" % (k, v) for k, v in self.options.items()],
+                    urls
+                ), len(urls)))
+        return self.results
+        
