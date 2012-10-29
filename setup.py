337e83b7a6cd067372d049a1c6a9313b42c6a617
Daniel Ryan <dryan@dryan.com>
2012-10-29 15:42:51 -0500

    revving version number

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def parse_dependency_links(file_name):
 
 setup(
     name                    =   'ccuapi',
-    version                 =   '0.1.0',
+    version                 =   '0.2.0',
     description             =   'Python wrapper around Akamai\'s Content Control Utility API',
     author                  =   'Daniel Ryan',
     author_email            =   'dryan@dryan.com',
