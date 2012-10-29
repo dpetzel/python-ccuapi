6bd59d8db3f852d5f05fd59c6516be2ff08e0285
Daniel Ryan <dryan@dryan.com>
2012-10-29 15:38:59 -0500

    fixing bug with using Django settings

--- ccuapi/__init__.py
+++ ccuapi/__init__.py
@@ -18,9 +18,9 @@ AKAMAI_NOTIFY_EMAIL =   os.environ.get('AKAMAI_NOTIFY_EMAIL', None)
 
 if django_settings:
     try:
-        AKAMAI_USERNAME     =   django_settings.get('AKAMAI_USERNAME', AKAMAI_USERNAME)
-        AKAMAI_PASSWORD     =   django_settings.get('AKAMAI_PASSWORD', AKAMAI_PASSWORD)
-        AKAMAI_NOTIFY_EMAIL =   django_settings.get('AKAMAI_NOTIFY_EMAIL', AKAMAI_NOTIFY_EMAIL)
+        AKAMAI_USERNAME     =   getattr(django_settings, 'AKAMAI_USERNAME', AKAMAI_USERNAME)
+        AKAMAI_PASSWORD     =   getattr(django_settings, 'AKAMAI_PASSWORD', AKAMAI_PASSWORD)
+        AKAMAI_NOTIFY_EMAIL =   getattr(django_settings, 'AKAMAI_NOTIFY_EMAIL', AKAMAI_NOTIFY_EMAIL)
     except ImportError:
         pass # this isn't being run inside a Django environment
 
