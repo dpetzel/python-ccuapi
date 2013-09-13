34dc6c65d7c6a38fba24b4b1f91fb8c6fbe66c86
Damian Fuentes <damianfuentes@livenation.com>
2013-09-13 14:48:29 -0700

    getting cpcodes to work and adding domain param for production or staging

--- ccuapi/ccu_purge.py
+++ ccuapi/ccu_purge.py
@@ -4,11 +4,10 @@ import ccuapi, optparse, sys, os
 PURGE_TYPES =   ['arl', 'cpcode']
 
 parser      =   optparse.OptionParser()
-parser.add_option('-p', '--production', dest = 'production', action = "store_true", default = True, help = "Invalidate the production property.")
-parser.add_option('-s', '--staging', dest = 'production', action = 'store_false', default = False, help = "Invalidate the staging property.")
+parser.add_option('-d', '--domain', help = "Where to purge from, 'production' or 'staging'")
 parser.add_option('-u', '--urls', help = "Path to a file of URLs or CP Codes to purge, one per line.")
 parser.add_option('-e', '--email', help = "Email addresses (comma-separated) to notify when purge is complete.")
-parser.add_option('--type', help = "The type of purge to perform. Choose from (%s)." % ", ".join(PURGE_TYPES), choices = PURGE_TYPES, default = 'arl', type = 'choice')
+parser.add_option('-t', '--type', help = "The type of purge to perform. Choose from 'arl' or 'cpcode'")
 parser.add_option('--username', help = "Akamai username to use.")
 parser.add_option('--password', help = "Akamai password to use.")
 
@@ -32,12 +31,18 @@ if opts.username:
     kwargs['username']  =   opts.username
 if opts.password:
     kwargs['password']  =   opts.password
+if opts.type:
+    kwargs['kind']      =   opts.type
+if opts.domain:
+    kwargs['domain']    =   opts.domain
+
 
 purger      =   ccuapi.purge.PurgeRequest(**kwargs)
 purger.add(urls)
 results     =   purger.purge()
 for result in results:
     if result[0].resultMsg == "Success.":
-        print '%d URL%s will be purged within %d minutes' % (result[1], '' if result[1] == 1 else 's', result[0].estTime / 60)
+        print '%d URL%s or CPcode[s] will be purged within %d minutes' % (result[1], '' if result[1] == 1 else 's', result[0].estTime / 60)
     else:
         print 'There was an error making the purge request: %s' % result[0].resultMsg if 'resultMsg' in result[0] else 'unknown error.'
+
